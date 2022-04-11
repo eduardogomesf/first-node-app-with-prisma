6d24c9ef722669338a01c3082af69204c93f3cca
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-04-11 19:51:24 -0300

    feat: add authenticate client endpoint

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "../modules/account/authenticateClient/authenticate-client.controller";
 import { CreateClientController } from "../modules/clients/useCases/createClient/create-client.controller";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
+const authenticateClientController = new AuthenticateClientController()
 
+routes.post('/authenticate/client', authenticateClientController.handle)
 routes.post('/client', createClientController.handle)
 
 export { routes }
