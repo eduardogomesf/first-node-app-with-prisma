7c633d94c5df91562cd80d0f358ea1ebd33c901e
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-04-11 20:08:52 -0300

    feat: add create deliveryman endpoint

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "../modules/account/authenticateClient/authenticate-client.controller";
 import { CreateClientController } from "../modules/clients/useCases/createClient/create-client.controller";
+import { CreateDeliverymanController } from "../modules/deliveryman/useCases/createDeliveryman/create-deliveryman.controller";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
 
 routes.post('/authenticate/client', authenticateClientController.handle)
 routes.post('/client', createClientController.handle)
+routes.post('/deliveryman', createDeliverymanController.handle)
 
 export { routes }
