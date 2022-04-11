edc84ad84b0fe54377d62e150f574b956bbd6336
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-04-11 19:19:07 -0300

    feat: add create client endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+import { routes } from './routes/routes'
 
 const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(routes)
 
 app.listen(3000, () => console.log(`Server running on port 3000`))
