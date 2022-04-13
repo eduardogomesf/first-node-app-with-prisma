ff3ef0d630544fa5949b14b5c37dc47219c81892
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-04-13 19:20:28 -0300

    feat: add endpoint for finding all deliveries available

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from "../modules/account/authenticateCli
 import { AuthenticateDeliverymanController } from "../modules/account/authenticateDeliveryman/authenticate-deliveryman.controller";
 import { CreateClientController } from "../modules/clients/useCases/createClient/create-client.controller";
 import { CreateDeliveryController } from "../modules/deliveries/useCases/createDelivery/create-delivery.controller";
+import { FindAllAvailableController } from "../modules/deliveries/useCases/findAllAvailable/find-all-available.controller";
 import { CreateDeliverymanController } from "../modules/deliveryman/useCases/createDeliveryman/create-deliveryman.controller";
 
 const routes = Router()
@@ -13,6 +14,7 @@ const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliveryController = new CreateDeliveryController()
+const findAllAvailableController = new FindAllAvailableController()
 
 routes.post('/authenticate/client', authenticateClientController.handle)
 routes.post('/authenticate/deliveryman', authenticateDeliverymanController.handle)
@@ -20,5 +22,6 @@ routes.post('/authenticate/deliveryman', authenticateDeliverymanController.handl
 routes.post('/client', createClientController.handle)
 routes.post('/deliveryman', createDeliverymanController.handle)
 routes.post('/delivery', ensureAuthenticatedClient, createDeliveryController.handle)
+routes.get('/delivery/available', findAllAvailableController.handle)
 
 export { routes }
